d644c00d1a403485a2d9ab0577ea1c4bbd97b375
easyutilsdev <79353864+easyutilsdev@users.noreply.github.com>
2021-04-07 21:07:39 +0900

    Add files via upload
    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by sel2 on 2021/04/07.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        print("Button was tapped!")
+    }
+    
+    
+    @IBAction func switchToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            print("Switch On")
+        } else {
+            print("Switch Off")
+        }
+    }
+        
+  
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    
+    
+}
+
