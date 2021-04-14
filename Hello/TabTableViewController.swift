8d1fc9e34ae00a9bd02c83b9a04dd28f23dd350a
JYP in MBP 15 <me@ebadaq.com>
2021-04-14 23:08:51 +0900

    버튼이 추가됨

--- Hello/TabTableViewController.swift
+++ Hello/TabTableViewController.swift
@@ -22,12 +22,14 @@ class TabTableViewController: UITableViewController {
         let label2 = cell.viewWithTag(2) as! UILabel
         let label3 = cell.viewWithTag(3) as! UILabel
         let viewImage = cell.viewWithTag(4) as! UIImageView
-
+        let btnGazua = cell.viewWithTag(5) as! UIButton
+        
         label1.text = "타이틀"
         label2.text = indexPath.description
         label3.text = "추가로 보이게 한다."
         
         viewImage.image = UIImage(systemName: "scribble")
+        btnGazua.setTitle((indexPath.row % 2 == 1) ? "좋아요!" : "싫어요", for: .normal)
         
         return cell
     }
