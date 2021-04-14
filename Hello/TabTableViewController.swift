9ac5bfe93c71247386b5647ff327736cb7b2544b
JYP in MBP 15 <me@ebadaq.com>
2021-04-14 23:22:39 +0900

    Custom cell added with outlet

--- Hello/TabTableViewController.swift
+++ Hello/TabTableViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 import Foundation
 
+class TabTableViewCell: UITableViewCell {
+    @IBOutlet weak var label1 :UILabel!
+    @IBOutlet weak var label2 :UILabel!
+    @IBOutlet weak var label3 :UILabel!
+    @IBOutlet weak var viewImage :UIImageView!
+    @IBOutlet weak var btnGazua :UIButton!
+}
+
 class TabTableViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -16,20 +24,14 @@ class TabTableViewController: UITableViewController {
         return 10
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1") as! TabTableViewCell
+                
+        cell.label1.text = "타이틀"
+        cell.label2.text = indexPath.description
+        cell.label3.text = "추가로 보이게 한다."
         
-        let label1 = cell.viewWithTag(1) as! UILabel
-        let label2 = cell.viewWithTag(2) as! UILabel
-        let label3 = cell.viewWithTag(3) as! UILabel
-        let viewImage = cell.viewWithTag(4) as! UIImageView
-        let btnGazua = cell.viewWithTag(5) as! UIButton
-        
-        label1.text = "타이틀"
-        label2.text = indexPath.description
-        label3.text = "추가로 보이게 한다."
-        
-        viewImage.image = UIImage(systemName: "scribble")
-        btnGazua.setTitle((indexPath.row % 2 == 1) ? "좋아요!" : "싫어요", for: .normal)
+        cell.viewImage.image = UIImage(systemName: "scribble")
+        cell.btnGazua.setTitle((indexPath.row % 2 == 1) ? "좋아요!" : "싫어요", for: .normal)
         
         return cell
     }
